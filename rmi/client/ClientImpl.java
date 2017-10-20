125e43bf8729f522429c69455567dc72062886e8
Przemysław Konik <przemyslawkonik@gmail.com>
2017-10-20 14:03:21 +0200

    Added client implementation

---
+++ rmi/client/ClientImpl.java
@@ -0,0 +1,24 @@
+package client;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import server.Server;
+
+public class ClientImpl implements Client {
+
+	public static void main(String[] args) {
+		System.setSecurityManager(new SecurityManager());
+
+		try {
+			Registry registry = LocateRegistry.getRegistry(args[0]);
+			Server server = (Server) registry.lookup(args[1]);
+			server.test(args[2]);
+		} catch (RemoteException | NotBoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
