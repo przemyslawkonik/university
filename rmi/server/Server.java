b9e2b10abea016fe3eb45b7d79d3b8d1d11bde0b
Przemysław Konik <przemyslawkonik@gmail.com>
2017-10-20 13:56:59 +0200

    Added server implementation

---
+++ rmi/server/Server.java
@@ -0,0 +1,8 @@
+package server;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Server extends Remote {
+	void test(String s) throws RemoteException;
+}
---
+++ rmi/server/ServerImpl.java
@@ -0,0 +1,29 @@
+package server;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class ServerImpl implements Server {
+
+	public static void main(String[] args) {
+		System.setSecurityManager(new SecurityManager());
+
+		try {
+			Server stub = (Server) UnicastRemoteObject.exportObject(new ServerImpl(), 0);
+			Registry registry = LocateRegistry.getRegistry();
+
+			registry.rebind(args[0], stub);
+			System.out.println("Server is on");
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void test(String s) {
+		System.out.println(s);
+	}
+}
